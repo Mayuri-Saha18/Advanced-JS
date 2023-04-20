b64e429aae671f8ce7457c70d6b849c3f76986ca
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-04-21 02:20:47 +0530

    add

--- recipe_app/scripts/recipre.js
+++ recipe_app/scripts/recipre.js
@@ -1,73 +1,75 @@
 
+
+
 let getData = async () => {
-let res = await fetch(`https://mock-server-6-6ljm.onrender.com/recipes`);
-let data = await res.json();
-appendData(data)
-console.log(data)
-}
+  let res = await fetch(`https://mock-server-6-6ljm.onrender.com/recipes`);
+  let data = await res.json();
+  appendData(data);
+  console.log(data);
+};
 getData();
 
 let cartLs = JSON.parse(localStorage.getItem("cart")) || [];
 let container = document.getElementById("home-container");
 
 function appendData(data) {
+  data.forEach(function (el, id) {
+    let div = document.createElement("div");
 
-    data.forEach(function(el,id) {
-       let div = document.createElement("div");
-        
-       let img = document.createElement("img");
-        img.src = el.image;
-        
-        let h2 = document.createElement("h2");
-        h2.innerText = el.title;
+    let img = document.createElement("img");
+    img.src = el.image;
 
-        let p1 = document.createElement("h2");
-        p1.innerText = el.description;
+    let h2 = document.createElement("h2");
+    h2.innerText = el.title;
 
-        let p2 = document.createElement("h2");
-        p2.innerText = el.category;
- 
+    let p1 = document.createElement("h2");
+    p1.innerText = el.description;
 
-        let btnEdit = document.createElement("button");
-        btnEdit.innerText = "Edit";
-        btnEdit.setAttribute("class","btn");
-        btnEdit.style.backgroundColor = "teal";
-        btnEdit.style.color = "white";
-        btnEdit.addEventListener("click",viewEdit);
-        function viewEdit(el,index) {
-            editFunc(el,index)
-        }
-     
-        let btnDelete = document.createElement("button");
-        btnDelete.innerText = "Delete";
-        btnEdit.setAttribute("class","btn");
-        btnDelete.style.backgroundColor = "red";
-        btnDelete.style.color = "white";
-        btnDelete.addEventListener("click",viewDelete);
-        function viewDelete (el) {
-            deleteRoom(el.id)
-        }
-     
+    let p2 = document.createElement("h2");
+    p2.innerText = el.category;
 
-      
-        div.append(img,h2,p1,p2,btnEdit,btnDelete);
-        container.append(div);
-    })
+    let p3 = document.createElement("h2");
+    p3.innerText = el.ingredients;
+
+    let p4 = document.createElement("h2");
+    p4.innerText = el.instructions;
+
+    let btnEdit = document.createElement("button");
+    btnEdit.innerText = "Edit";
+    btnEdit.setAttribute("class", "btn");
+    btnEdit.style.backgroundColor = "teal";
+    btnEdit.style.color = "white";
+    btnEdit.addEventListener("click", () => viewEdit(el.id, id));
+
+    let btnDelete = document.createElement("button");
+    btnDelete.innerText = "Delete";
+    btnDelete.setAttribute("class", "btn");
+    btnDelete.style.backgroundColor = "red";
+    btnDelete.style.color = "white";
+    btnDelete.addEventListener("click", () => viewDelete(el.id));
+
+    div.append(img, h2, p1, p2,p3,p4, btnEdit, btnDelete);
+    container.append(div);
+  });
 }
 
+// Delete 
+
+function viewDelete(id) {
+  if (confirm("Are you sure you want to delete this recipe?")) {
+    deleteRoom(id);
+  }
+}
 const deleteRoom = async (id) => {
-    const response = await fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${id}`, {
-      method: "DELETE",
-    });
-    await response.json();
-    getData(response)
-  };
+  const response = await fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${id}`, {
+    method: "DELETE",
+  });
+  await response.json();
+  window.location.reload();
+  getData(response);
+};
 
-
-
-function editFunc() {
-
-}
+// add data 
 
 const recipeForm = document.getElementById('recipe-form');
 
@@ -105,84 +107,87 @@ function addRecipe(recipe) {
   })
   .then(response => response.json())
   .then(data => console.log(data))
-  .catch(error => console.error(error));
+  .catch(error => console.log(error));
 }
 
 
-//  edit
+// edit
 
-const editRecipeModal = document.getElementById('edit-recipe-modal');
-const editRecipeForm = document.getElementById('edit-recipe-form');
-const editRecipeTitle = document.getElementById('edit-recipe-title');
-const editRecipeImage = document.getElementById('edit-recipe-image');
-const editRecipeDescription = document.getElementById('edit-recipe-description');
-const editRecipeCategory = document.getElementById('edit-recipe-category');
-const editRecipeIngredients = document.getElementById('edit-recipe-ingredients');
-const editRecipeInstructions = document.getElementById('edit-recipe-instructions');
+const editRecipeModal = document.getElementById("edit-recipe-modal");
+const editRecipeForm = document.getElementById("edit-recipe-form");
+const editRecipeTitle = document.getElementById("edit-recipe-title");
+const editRecipeImage = document.getElementById("edit-recipe-image");
+const editRecipeDescription = document.getElementById("edit-recipe-description");
+const editRecipeCategory = document.getElementById("edit-recipe-category");
+const editRecipeIngredients = document.getElementById("edit-recipe-ingredients");
+const editRecipeInstructions = document.getElementById("edit-recipe-instructions");
 
 // Display the edit recipe modal
-function displayEditRecipeModal(recipeId) {
+function viewEdit(id, index) {
   // Get the recipe data from the JSON server using a GET request
-  fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${recipeId}`)
-    .then(response => response.json())
-    .then(recipe => {
+  fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${id}`)
+    .then((response) => response.json())
+    .then((recipe) => {
       // Populate the form fields with the recipe data
       editRecipeTitle.value = recipe.title;
       editRecipeImage.value = recipe.image;
-      editRecipeDescription.value = recipe.description;
+      editRecipeDescription.value = recipe.description
       editRecipeCategory.value = recipe.category;
-      editRecipeIngredients.value = recipe.ingredients.join(', ');
+      editRecipeIngredients.value = recipe.ingredients.join("\n");
       editRecipeInstructions.value = recipe.instructions;
-      editRecipeForm.setAttribute('data-id', recipe.id);
 
-      // Show the edit recipe modal
-      editRecipeModal.style.display = 'block';
+      // Display the edit recipe modal
+      editRecipeModal.style.display = "block";
+
+      // When the user submits the edit recipe form
+      editRecipeForm.onsubmit = (event) => {
+        event.preventDefault();
+
+        // Get the updated recipe data from the form fields
+        const updatedRecipe = {
+          title: editRecipeTitle.value,
+          image: editRecipeImage.value,
+          description: editRecipeDescription.value,
+          category: editRecipeCategory.value,
+          ingredients: editRecipeIngredients.value.split("\n"),
+          instructions: editRecipeInstructions.value,
+        };
+
+        // Send a PUT request to the JSON server with the updated recipe data
+        fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedRecipe),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // Update the recipe data in the UI
+            const recipeDiv = container.children[index];
+            recipeDiv.children[0].src = updatedRecipe.image;
+            recipeDiv.children[1].innerText = updatedRecipe.title;
+            recipeDiv.children[2].innerText = updatedRecipe.description;
+            recipeDiv.children[3].innerText = updatedRecipe.category;
+            recipeDiv.children[4].innerText = updatedRecipe.ingredients;
+            recipeDiv.children[5].innerText = updatedRecipe.instructions;
+
+            // Hide the edit recipe modal
+            // const closeModal = document.createElement('span');
+            // closeModal.classList.add('close');
+            // closeModal.innerHTML = '&times;';
+            // closeModal.addEventListener('click', () => {
+            //   editRecipeModal.style.display = 'none';
+            // });
+            editRecipeModal.style.display = "none";
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
     })
-    .catch(error => console.error(error));
+    .catch((error) => {
+      console.log(error);
+    });
 }
-
-// Hide the edit recipe modal when the close button is clicked
-editRecipeModal.querySelector('.close-button').addEventListener('click', () => {
-  editRecipeModal.style.display = 'none';
-});
-
-// Update the recipe on the server when the form is submitted
-editRecipeForm.addEventListener('submit', event => {
-  event.preventDefault();
-  
-  const recipeId = editRecipeForm.getAttribute('data-id');
-  const updatedRecipe = {
-    title: editRecipeTitle.value,
-    image: editRecipeImage.value,
-    description: editRecipeDescription.value,
-    category: editRecipeCategory.value,
-    ingredients: editRecipeIngredients.value.split(','),
-    instructions: editRecipeInstructions.value
-  };
-
-  // Send a PUT/PATCH request to update the recipe on the server
-  fetch(`https://mock-server-6-6ljm.onrender.com/recipes/${recipeId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(updatedRecipe)
-  })
-    .then(response => response.json())
-    .then(recipe => {
-      // Update the recipe card with the updated data
-      const recipeCard = document.getElementById(`recipe-${recipe.id}`);
-      recipeCard.querySelector('.recipe-title').textContent = recipe.title;
-      recipeCard.querySelector('.recipe-image').setAttribute('src', recipe.image);
-      recipeCard.querySelector('.recipe-description').textContent = recipe.description;
-      recipeCard.querySelector('.recipe-category').textContent = recipe.category;
-      recipeCard.querySelector('.recipe-ingredients').textContent = recipe.ingredients.join(', ');
-      recipeCard.querySelector('.recipe-instructions').textContent = recipe.instructions;
-
-      // Hide the edit recipe modal
-      editRecipeModal.style.display = 'none';
-    })
-    .catch(error => console.error(error));
-});
-
 
